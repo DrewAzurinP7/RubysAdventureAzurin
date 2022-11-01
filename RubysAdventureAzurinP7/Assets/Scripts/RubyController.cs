b0ec3e71211db34221d7340275ec5a1982f4b3d1
DrewAzurinP7 <740723@student.dusd.net>
2022-11-01 15:25:22 -0700

    Added Tile map to Ruby's Adventure
    I added the background for Ruby's Adventure

--- RubysAdventureAzurinP7/Assets/Scripts/RubyController.cs
+++ RubysAdventureAzurinP7/Assets/Scripts/RubyController.cs
@@ -13,8 +13,12 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
-        position.x = position.x + 0.01f;
+        position.x = position.x + 0.01f * horizontal;
+        position.y = position.y + 0.01f * vertical;
         transform.position = position;
     }
 }
